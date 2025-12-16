208c1e0d6348d585026c3e017705d93ed08a3811
Neelam Raikwar <neelam.raikwar.234303@gmail.com>
2025-12-16 18:35:06 +0530

    fix: added a new PORT

--- index.js
+++ index.js
@@ -214,6 +214,6 @@ app.get("/users", async (req, res) => {
 // const PORT = process.env.PORT || 5001;
 // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
